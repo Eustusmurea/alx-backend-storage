960362faba27158968694f427a936334a6ceaca1
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2024-03-23 22:58:01 +0300

    pld session corrections

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,5 @@
 -- Select origin column from metal_band table
---count the number of fans for each origin
---order by number of fans in descending order
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
