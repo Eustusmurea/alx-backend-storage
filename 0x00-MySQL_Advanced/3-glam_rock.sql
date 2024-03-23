1a5682bb49c3e6dc561965bbca64484c519d1103
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2024-03-23 23:34:12 +0300

    Task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Select the band name, the year it was formed, and the lifespan of the band
 SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan, 
 FROM metal_bands
-WHERE style LIKE '%glam rock%'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
