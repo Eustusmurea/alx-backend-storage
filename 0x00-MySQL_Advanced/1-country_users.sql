79d78d9f1cef565ac6e0560c40b7c5c039e3236d
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2024-03-23 22:51:12 +0300

    add files

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,7 @@
 -- Create a table named users with the following columns:
-CREATE TABLE users (
+drop table users;
+
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
