5bb581bc351b113a143dfe44f3e441ee32ee6310
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2024-03-23 23:31:57 +0300

    CORRECT

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,4 @@
 -- Create a table named users with the following columns:
-DROP TABLE users IF EXISTS;
-
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
