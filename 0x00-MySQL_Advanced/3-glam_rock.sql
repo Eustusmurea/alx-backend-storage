390868330f93618f4fe3098b1e17d1e5ea219f6e
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2024-03-23 23:27:39 +0300

    edits

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,5 @@
 -- Select the band name, the year it was formed, and the lifespan of the band
-SELECT 
-    band_name,
-    YEAR(date_formed) AS formed_year, 
-    YEAR(SUBSTRING_INDEX(lifespan, '-')[2]) - YEAR(SUBSTRING _INDEX(lifespan, '–')[1]) AS lifespan  -- Changed '-' to '–' for consistency
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan, 
 FROM metal_bands
-WHERE main_style = 'Glam Rock'
-ORDER BY 
-    lifespan DESC;
+WHERE style LIKE '%glam rock%'
+ORDER BY lifespan DESC;
