d6c3b8125a86f5b2b2d07b90e260bf65693fe816
Eustus Murea <mwirigieustus97@gmail.com>
2024-03-25 08:31:15 -0700

    Add dump

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
+'''Task 8's module.
+'''
 
-from pymongo import MongoClient
 
-# Connect to MongoDB
-client = MongoClient('mongodb://localhost:27017/')
-db = client.my_db  # Specify the database name
-collection = db.school  # Specify the collection name
-
-# Call the function to list all documents
-documents_list = list_all(collection)
-
-# Print the documents
-for document in documents_list:
-    print(document)
+def list_all(mongo_collection):
+    '''Lists all documents in a collection.
+    '''
+    return [document for document in mongo_collection.find()]
 
