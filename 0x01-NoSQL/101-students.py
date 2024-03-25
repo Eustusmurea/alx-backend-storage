c97fecf198c9f093b36ff10464e9ac9b652543c4
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-03-25 19:27:56 +0300

    Updates

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 '''Task 14's module.
 '''
-
-
 def top_students(mongo_collection):
     '''Prints all students in a collection sorted by average score.
     '''
@@ -26,4 +24,3 @@ def top_students(mongo_collection):
         ]
     )
     return students
-
--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,9 +2,7 @@
 '''Task 8's module.
 '''
 
-
 def list_all(mongo_collection):
     '''Lists all documents in a collection.
     '''
     return [document for document in mongo_collection.find()]
-
